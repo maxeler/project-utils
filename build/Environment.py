ba7b1ac8fac1179de04138be8e08a9eb7500d39c
Itay Greenspon <itay.gr@gmail.com>
2016-04-11 11:45:26 +0100

    Print INFO/WARN/ERROR prefix

--- build/Environment.py
+++ build/Environment.py
@@ -4,28 +4,28 @@ import sys
 
 def require(key):
 	if not key in os.environ:
-		print "Required environment variable not set: %s" % (key)
+		print "[ERROR] Required environment variable not set: %s" % (key)
 		sys.exit(1)
 	val = os.environ.get(key)
-	print "Required Environment variable: %s = %s" % (key, val)
+	print "[INFO] Required Environment variable: %s = %s" % (key, val)
 	return val
 
 
 def optional(key):
 	if not key in os.environ:
-		print "Optional environment variable not set: %s" % (key)
+		print "[WARN] Optional environment variable not set: %s" % (key)
 		return ""
 	
 	val = os.environ.get(key)
-	print "Optional Environment variable: %s = %s" % (key, val)
+	print "[INFO] Optional Environment variable: %s = %s" % (key, val)
 	return val 
 
 
 def set(key, value):
 	if value:
-		print "Setting environment %s <-- %s" % (key, value)
+		print "[INFO] Setting environment %s <-- %s" % (key, value)
 		os.environ[key] = value
 	else:
-		print "Unsetting environment variable %s" % (key)
+		print "[INFO] Unsetting environment variable %s" % (key)
 		del os.environ[key]
 
